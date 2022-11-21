117b0c65c0439c56477438e6c10503406f794f15
atul <atulrane04@gmail.com>
2022-11-21 23:14:37 +0530

    First commit

---
+++ src/com/demo/features/Main.java
@@ -0,0 +1,9 @@
+package com.demo.features;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is java 8 feature series. Daily one feature promised for blog.");
+    }
+}
